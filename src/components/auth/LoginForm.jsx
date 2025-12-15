045db0ded4158faca27744a8674cde651256a7b1
Nahid Hossain <nahid4510@gmail.com>
2025-12-15 23:46:57 +0600

    login form created using react-hook-form

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Field from '../common/Field';
+
+const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+
+    } = useForm();
+
+    const submitForm = (formData) => {
+        console.log(formData);
+        
+    }
+    return (
+       <form action="" className='border-b border-[#3F3F3F] pb-10 lg:pb-[60px]' onSubmit={handleSubmit(submitForm)}>
+        <Field label="Email">
+            <input {...register("email", {required: "Email ID is required"})} type="email" name='email' id='email' className={`auth-input ${errors.email ? "border-red-500" : "border-gray-200"}`} />
+        </Field>
+
+        <Field lable="Password">
+            
+             <input {...register("password", {required: "Password is required",
+                minLength: {
+                    value: 8,
+                    message: "Your password must be at least 8 characters"
+                }
+             },)} type="password" name='password' id='password' className={`auth-input ${errors.password ? "border-red-500" : "border-gray-200"}`} />
+        </Field>
+        <Field>
+             <button
+                class="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+              >
+                Login
+              </button>
+        </Field>
+       </form>
+    );
+};
+
+export default LoginForm;
