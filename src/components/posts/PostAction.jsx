2c4a49c894782da51afa0ac581346ddc58102977
Nahid Hossain <nahid4510@gmail.com>
2025-12-17 17:36:17 +0600

    like and comment functionality done

--- src/components/posts/PostAction.jsx
+++ src/components/posts/PostAction.jsx
@@ -1,32 +1,53 @@
-import React from 'react';
-import LikeIcon from '../../assets/icons/like.svg'
+import React, { useState } from 'react';
+import LikeIcon from '../../assets/icons/like.svg';
+import FilledLikeIcon from '../../assets/icons/filled-like.svg';
 import CommentIcon from '../../assets/icons/comment.svg'
 import ShareIcon from '../../assets/icons/share.svg'
+import { useAxios } from '../../hooks/useAxios';
+import { useAuth } from '../../hooks/useAuth';
 
-const PostAction = ({postId, commentCount}) => {
+const PostAction = ({ post, commentCount }) => {
+    const { auth } = useAuth();
+    const [liked, setLiked] = useState(post?.likes?.includes(auth?.user?.id));
+    const { api } = useAxios();
+
+    const handleLike = async () => {
+        try {
+            const response = await api.patch(`${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/like`);
+
+            if (response.status === 200) {
+                setLiked(!liked)
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
     return (
-         <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
-                <button
-                    className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
-                >
-                    <img src={LikeIcon} alt="Like" />
-                    <span>Like</span>
-                </button>
+        <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
+             <button
+              className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
+                onClick={handleLike}
+            >
+              <img className='w-6' src={ liked ? FilledLikeIcon : LikeIcon} alt="Like" />
+              {!liked && <span>Like</span>}
+            </button>
 
-                <button
-                    className="icon-btn space-x-2 px-6 py-3 text-xs lg:px-12 lg:text-sm"
-                >
-                    <img src={CommentIcon} alt="Comment" />
-                    <span>Comment({commentCount ?? 0})</span>
-                </button>
 
-                <button
-                    className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
-                >
-                    <img src={ShareIcon} alt="Share" />
-                    <span>Share</span>
-                </button>
-            </div>
+            <button
+                className="icon-btn space-x-2 px-6 py-3 text-xs lg:px-12 lg:text-sm"
+            >
+                <img src={CommentIcon} alt="Comment" />
+                <span>Comment({commentCount ?? 0})</span>
+            </button>
+
+            <button
+                className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
+            >
+                <img src={ShareIcon} alt="Share" />
+                 <span>Share</span>
+            </button>
+        </div>
     );
 };
 
--- src/components/posts/PostComments.jsx
+++ src/components/posts/PostComments.jsx
@@ -1,16 +1,38 @@
 import React, { useState } from 'react';
 import { useAvatar } from '../../hooks/useAvatar';
 import PostCommentList from './PostCommentList';
+import { useAxios } from '../../hooks/useAxios';
+import { useAuth } from '../../hooks/useAuth';
 
 const PostComments = ({ post }) => {
     const { avatarURL } = useAvatar(post);
+    const [comments, setComments] = useState(post?.comments);
+    const [comment, setComment] = useState();
+    const { api } = useAxios();
+    const { auth } = useAuth();
 
+    const addComment = async (e) => {
+        const keyCode = e.keyCode;
+
+        if(keyCode === 13){
+           try {
+             const response = await api.patch(`${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id}/comment`, {comment});
+
+            if(response.status === 200) {
+                setComments([...response.data.comments])
+            }
+           } catch (error) {
+            console.log(error);
+            
+           }
+        }
+    }
     return (
         <div>
             <div className="flex-center mb-3 gap-2 lg:gap-4">
                 <img
                     className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                    src={avatarURL}
+                    src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
                     alt="avatar"
                 />
 
@@ -20,12 +42,15 @@ const PostComments = ({ post }) => {
                         className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
                         name="post"
                         id="post"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        onKeyDown={(e) => addComment(e)}
                         placeholder="What's on your mind?"
                     />
                 </div>
             </div>
                
-                    <PostCommentList comments={post?.comments} />
+                    <PostCommentList comments={comments} />
                 
             
         </div>
